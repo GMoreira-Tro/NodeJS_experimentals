9fbfbab0e43922494968828ac6e4dcaad8ae6359
Moreira <guilherme.dsmoreira@gmail.com>
2021-12-06 15:17:23 -0300

    bastante coisa, ein

--- OhayoSekai.js
+++ OhayoSekai.js
@@ -1,6 +1,8 @@
 //#region Módulos
 const APISample = require("./APISample")
 const http = require('http')
+const express_module = require("express")
+const express = express_module()
 //#endregion
 
 //#region Valores constantes
@@ -10,8 +12,24 @@ const PORT = 8081
 APISample.something()
 APISample.something2(3.1415926537)
 
-http.createServer((req,res) => 
+express.get("/Aqui", (_req,res) => 
 {
-    res.end("oioioi :3 ´~^")
-}).listen(PORT)
-console.log("Server aberto ´~^")
+	message = ""
+	for(i = 0; i < 1000000; i++)
+	{
+		message += "Te amo, Suâne\n"
+	}
+	res.send(message)
+})
+express.get("/Dois/:nome/:tamanho_do_pau", (req,res) =>
+{
+	res.send("<h1>Nome: " + req.params.nome +
+	"</h1><h1>Tamanho do pito: " + req.params.tamanho_do_pau + "</h1>")
+})
+express.get("/html", (req,res) =>
+{
+	res.sendFile(__dirname + "/HTML/index.html")
+})
+
+express.listen(PORT)
+
